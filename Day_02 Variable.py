41c57c990e527602d55422fded2d77c7f847fcc1
AbdurRahim666 <113165590+AbdurRahim666@users.noreply.github.com>
2022-09-16 16:45:02 +0530

    variable.py

---
+++ Day_02 Variable.py
@@ -0,0 +1,37 @@
+# Day 2:30 days of python programming
+Firts_name = 'Asebeneh'
+Last_name = 'Feetwala'
+Country = 'Canada'
+City = 'Helsinki'
+Age = 250
+is_married = True
+skills = ['HTML' ,'CSS' ,'JAVA' ,'PYTHON' ,'JS']
+personal_info {
+  'First_name':'Asabeneh'
+  'Last_name':'Feetwala'
+  'Country':'Canada'
+  'City':'Helsinki'
+}
+# Printing the values stored in the variables
+print('First_name:', first_name')
+print('First name lenght:' , len(first_name))
+print('Last_name:', last_name')
+print('Last_name Lenght:', len(last_name))
+print('Country:' country)
+print('City:' city)
+print('Age:' age)
+print('Married:' is_married)      
+print('Skills: ', skills)      
+
+print('Person information: ', person_info)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)  
